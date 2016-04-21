17cb282eaa2f6cf203c65ebf353758b6431911c6
Morad Edwar <me@morad-edwar.com>
2016-04-21 16:11:26 +0200

    1.2 Setting Banner Ad

--- app/src/main/java/mal/helloadmob/MainActivity.java
+++ app/src/main/java/mal/helloadmob/MainActivity.java
@@ -3,11 +3,17 @@ package mal.helloadmob;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
